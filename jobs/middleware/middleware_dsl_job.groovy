fd4e6655d62618b280570f639726d41313ad0d0c
Sumeet Raheja <sumeetraheja@ip-10-15-0-179.eu-west-1.compute.internal>
2018-05-14 12:54:10 +0100

    Devops Jobs

--- jobs/middleware/middleware_dsl_job.groovy
+++ jobs/middleware/middleware_dsl_job.groovy
@@ -1,17 +1,16 @@
-node {
-  
-   stage('Preparation') { // for display purposes
-      // Get some code from a GitHub repository
-      git 'https://github.com/sumeetraheja/jenkins.git'
-      echo "workspace directory is ${workspace}"
-
-     
-      jobDsl targets: ['jobs/*.groovy'].join('\n'),
-         removedJobAction: 'DELETE',
-         removedViewAction: 'DELETE',
-         lookupStrategy: 'SEED_JOB',
-         additionalClasspath: ['target/jenkins-job-0.0.1-SNAPSHOT.jar'].join('\n') 
-         
-  }
-  
+job('middleware') {
+   multiscm {
+        git {
+            remote {
+                github('https://github.com/sumeetraheja/seed-jobs.git')
+              }
+           
+           }
+      }  
+    steps {
+        dsl {
+            text(readFileFromWorkspace('jobs/devSeedJob.groovy'))
+            additionalClasspath('target/jenkins-job-0.0.1-SNAPSHOT.jar')
+        }
+    }
 }
