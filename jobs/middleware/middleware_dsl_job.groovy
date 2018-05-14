9a4f48331d6429a667a748c3fe9f9a50e850c7e9
Sumeet Raheja <sumeetraheja@ip-10-15-0-179.eu-west-1.compute.internal>
2018-05-14 13:52:53 +0100

    Devops Jobs

--- jobs/middleware/middleware_dsl_job.groovy
+++ jobs/middleware/middleware_dsl_job.groovy
@@ -10,6 +10,7 @@ job('middleware') {
     steps {
         dsl {
             text(readFileFromWorkspace('jobs/devSeedJob.groovy'))
+            external('jobs/devSeedJob.groovy')
             additionalClasspath('target/jenkins-job-0.0.1-SNAPSHOT.jar')
         }
     }
