80889754c5c426218ef25cc61ea5b1a687f2d4ca
Sumeet Raheja <sumeetraheja@ip-10-15-24-145.eu-west-1.compute.internal>
2018-05-15 11:16:44 +0100

    Devops Jobs

--- views/middleware/projects_view.groovy
+++ views/middleware/projects_view.groovy
@@ -3,7 +3,8 @@ listView('middleware') {
     filterBuildQueue()
     filterExecutors()
     jobs {       
-        names('middleware-jobs-commons','middleware-jobs-commons-all-env','middleware-jobs-dev','middleware-jobs-production','middleware-jobs-staging')
+        //names('middleware-jobs-commons','middleware-jobs-commons-all-env','middleware-jobs-dev','middleware-jobs-production','middleware-jobs-staging')
+        regex('middleware-jobs')
     }
     columns {
         status()
